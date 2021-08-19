d1b771e0d12aef0c5ebc76d7cffa3885c0ae7aa2
HyojinKim <gywls00100@gmail.com>
2021-08-20 00:36:26 +0900

    progress bar 완성

--- 3Dwall/js/wall3d.js
+++ 3Dwall/js/wall3d.js
@@ -1,11 +1,26 @@
 (function () {
   const stageElem = document.querySelector(".stage");
   const houseElem = document.querySelector(".house");
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  const barElem = document.querySelector(".progress-bar");
+
+  let maxScrollValue;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
   window.addEventListener("scroll", function () {
     const scrollPer = pageYOffset / maxScrollValue;
     const zMove = scrollPer * 980 - 490;
     houseElem.style.transform = "translateZ(" + zMove + "vw)";
+
+    //progress bar
+    barElem.style.width = scrollPer * 100 + "%";
   });
+
+  window.addEventListener('mousemove', function() {
+      
+  })
+
+  window.addEventListener("resize", resizeHandler);
 })();
