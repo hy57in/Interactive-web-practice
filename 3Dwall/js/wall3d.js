88aa8fd89f5c96f40a36893a7625bd87815f9984
HyojinKim <gywls00100@gmail.com>
2021-08-20 00:50:16 +0900

    무대 완성

--- 3Dwall/js/wall3d.js
+++ 3Dwall/js/wall3d.js
@@ -2,7 +2,7 @@
   const stageElem = document.querySelector(".stage");
   const houseElem = document.querySelector(".house");
   const barElem = document.querySelector(".progress-bar");
-
+  const mousePos = { x: 0, y: 0 };
   let maxScrollValue;
 
   function resizeHandler() {
@@ -18,9 +18,12 @@
     barElem.style.width = scrollPer * 100 + "%";
   });
 
-  window.addEventListener('mousemove', function() {
-      
-  })
+  window.addEventListener("mousemove", function (e) {
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageElem.style.transform =
+      "rotateX(" + mousePos.y * 5 + "deg) rotateY(" + mousePos.x * 5 + "deg)";
+  });
 
   window.addEventListener("resize", resizeHandler);
 })();
